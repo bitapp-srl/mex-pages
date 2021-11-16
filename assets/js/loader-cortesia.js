89a05fd37deb9cfcde8962580f2a4ed675dd3e2e
Andrea Martini <andrea.martini@bitapp.it>
2021-11-16 18:44:47 +0100

    API url

--- assets/js/loader-cortesia.js
+++ assets/js/loader-cortesia.js
@@ -1,6 +1,6 @@
 var loader = function loader() {
     var stop = window.location.hash.replace('#', '');
-    $.get('https://mex.bitapp.it/load.php?' + stop, function(response) {
+    $.get('https://api.mex.bitapp.it/load.php?' + stop, function(response) {
         if (response.active) {
             window.location.href = 'index.html#' + stop;
         }
